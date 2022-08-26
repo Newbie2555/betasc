75333b9ec3d4669046b336a2953e1be6381960a0
Newbie2555 <107703349+Newbie2555@users.noreply.github.com>
2022-08-26 13:22:07 +0700

    Update about.sh

--- ssh/about.sh
+++ ssh/about.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 clear
-echo -e "=====================================" | lolcat
-echo -e "Premium Script Remodified By WaanStore"
-echo -e "Script ini masih beta gak work all vpn service"
-echo -e "Harap ganti domain terlebih dahulu"
-echo -e "=====================================" | lolcat
-echo -e
-echo -e "Thanks To :" | lolcat
-echo -e "LamVpn"
-echo -e "Horas"
-echo -e "Allah Swt"
-echo -e "=====================================" | lolcat
+echo -e "
+Wellcome to premium AUTOSC
+Script ini telah di modifikasi
+Oleh WaanStore
+
+Thanks To : 
+Allah SWT
+LamVpn
+Horas
+Sulaiman L"
